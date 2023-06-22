b53380e67d43aec9e987cbeb1ff192fbf8d01a8f
unknown <shubhamsinghal12@gmail.com>
2023-06-22 13:40:38 +0530

    Lecture 10

--- Lec10/SubArraySumEqualsK.java
+++ Lec10/SubArraySumEqualsK.java
@@ -6,5 +6,29 @@ public class SubArraySumEqualsK {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public static int subarraySum(int[] nums, int k) {
+		
+		int[] prefixA = new int[nums.length+1];
+		for(int i = 0; i < nums.length; i++)
+		{
+			prefixA[i+1] = prefixA[i]+nums[i]; 
+		}
+		
+		int count = 0;
+		for(int i = 1; i < prefixA.length; i++)
+		{
+			for(int j = 0; j < i; j++)
+			{
+				if(prefixA[i]-prefixA[j] == k)
+				{
+					count++;
+				}
+			}
+		}
+		return count;
+		
+		
+	}
 
 }
